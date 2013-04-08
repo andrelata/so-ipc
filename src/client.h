ff1f1423405fcfbcafbd753281b5d4c33e3cb9de
Gonzalo  Castro Pena <gcastrop@lab94.it.itba.edu.ar>
2013-04-08 15:25:57 -0300

    compila

--- src/client.h
+++ src/client.h
@@ -1,3 +1,15 @@
 #define	FALSE	0
 #define TRUE	1
 #define bool	int
+
+bool connect(char* nickname);
+
+void disconnect();
+
+char * getSessions();
+
+void joinSession(int sessionID);
+
+void createSession(char* name);
+
+char * readText();
---
+++ src/fifo.c~
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/stat.h>
+
+void
+fatal(char *s)
+{
+	perror(s);
+	exit(1);
+}
+
+int
+main(int argc, char **argv)
+{
+	
+}
+
